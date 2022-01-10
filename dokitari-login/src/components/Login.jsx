1e0e005becca4f4027debe9d9cef4a581c620ac3
Bukola <bukolaebikefe@yahoo.com>
2022-01-10 15:46:44 +0100

    basestyles for form

--- dokitari-login/src/components/Login.jsx
+++ dokitari-login/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const baseInputStyle =
-  "ring-1 ring-gray-500 rounded-md py-1 px-2 outline-transparent mt-2";
+  "ring-1 ring-gray-500 rounded-md py-2 px-2 focus:outline-none mt-2 text-sm focus:ring-1 focus:ring-blue-400";
 
 const Login = () => {
   const [emailValue, setEmail] = useState("");
@@ -10,11 +10,12 @@ const Login = () => {
 
   return (
     <div className="w-full sm:w-1/3 md:w-1/4">
-      <form className="flex flex-col mt-8 w-full">
+      <form className="flex flex-col mt-8 w-full ring-1 ring-gray-400 rounded-md px-4 py-5 ">
         <Label id="email" label="Email Address" />
         <input
           type="email"
           id="email"
+          placeholder="Enter your email"
           value={emailValue}
           onChange={(e) => setEmail(e.target.value)}
           className={`${baseInputStyle}`}
@@ -23,6 +24,7 @@ const Login = () => {
         <input
           type="password"
           id="password"
+          placeholder="Enter your password"
           value={passwordValue}
           className={`${baseInputStyle}`}
           onChange={(e) => setPassword(e.target.value)}
@@ -31,11 +33,17 @@ const Login = () => {
         <input
           type="password"
           id="confirmPassword"
+          placeholder="Confirm password"
           value={checkPassword}
           className={`${baseInputStyle}`}
           onChange={(e) => setCheckPassword(e.target.value)}
         />
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          className="bg-blue-200 w-24 text-center rounded-md p-2 text-white font-bold mt-2 place-self-end"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
@@ -45,7 +53,7 @@ export default Login;
 
 const Label = ({ id, label }) => {
   return (
-    <label htmlFor={id} className="font-semibold">
+    <label htmlFor={id} className="font-semibold mt-2">
       {label}
     </label>
   );
