cae74bfe53423a532a0ac0e7a06ac60f8c12e11c
Bukola <bukolaebikefe@yahoo.com>
2022-01-17 21:46:19 +0100

    redirect to chatpage

--- dokitari-login/src/App.jsx
+++ dokitari-login/src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import Login from "./pages/Login";
 import ChatPage from "./pages/ChatPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App h-screen w-screen overflow-x-hidden">
-      <Login />
-      {/* <ChatPage /> */}
-    </div>
+    <Router>
+      <div className="App h-screen w-screen overflow-x-hidden">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- dokitari-login/src/pages/Login.jsx
+++ dokitari-login/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ErrorMessage from "../components/ErrorMessage";
+import { Navigate } from "react-router-dom";
 
 const baseInputStyle =
   "ring-1 ring-gray-500 rounded-md py-2 px-2 focus:outline-none mt-2 text-sm focus:ring-1 focus:ring-blue-400";
@@ -11,6 +12,7 @@ const Login = () => {
   const [validEmail, setValidEmail] = useState(true);
   const [passwordWeak, setPasswordWeak] = useState(false);
   const [passwordMatch, setPasswordMatch] = useState(true);
+  const [redirect, setRedirect] = useState(null);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -69,70 +71,75 @@ const Login = () => {
     handleEmailValue();
     handlePasswordValue();
     handleMatchPassword(checkPassword);
+    setRedirect("/Chat");
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center mt-24 w-full px-4">
-      <p className="text-blue-500 text-2xl font-semibold">Sign In</p>
-      <div className="w-full sm:w-1/3">
-        <form
-          className="flex flex-col mt-8 w-full ring-1 ring-gray-400 rounded-md px-4 py-5 mb-4"
-          onSubmit={handleSubmit}
-        >
-          <Label id="email" label="Email Address" />
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your email"
-            value={emailValue}
-            onChange={(e) => handleEmailChange(e)}
-            className={`${baseInputStyle} ${
-              !validEmail ? "focus:ring-red-400" : "focus:ring-blue-400"
-            }`}
-          />
-          {!validEmail && (
-            <ErrorMessage message="Please input a valid email :/" />
-          )}
-          <Label id="password" label="Password" />
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter your password"
-            value={passwordValue}
-            autoComplete="true"
-            className={`${baseInputStyle} ${
-              passwordWeak && "focus:ring-red-400"
-            }`}
-            onChange={(e) => handlePasswordChange(e)}
-          />
-          {passwordWeak && (
-            <ErrorMessage message="A strong password should be 8-15 characters with at least one uppercase, one lowercase, one numeric digit and one special character" />
-          )}
-          <Label id="confirmPassword" label="Confirm Password" />
-          <input
-            type="password"
-            id="confirmPassword"
-            placeholder="Confirm password"
-            autoComplete="true"
-            value={checkPassword}
-            className={`${baseInputStyle} ${
-              !passwordMatch && "focus:ring-red-400"
-            }`}
-            onChange={(e) => handleConfirmPassword(e)}
-          />
-          {!passwordMatch && (
-            <ErrorMessage message="Passwords don't match, please crosscheck." />
-          )}
-          <button
-            type="submit"
-            onClick={(e) => handleSubmit(e)}
-            className="bg-blue-400 w-24 text-center rounded-md p-2 text-white font-bold mt-4 place-self-end"
+  return redirect ? (
+    <Navigate to={redirect} />
+  ) : (
+    <>
+      <div className="flex flex-col items-center justify-center mt-24 w-full px-4">
+        <p className="text-blue-500 text-2xl font-semibold">Sign In</p>
+        <div className="w-full sm:w-1/3">
+          <form
+            className="flex flex-col mt-8 w-full ring-1 ring-gray-400 rounded-md px-4 py-5 mb-4"
+            onSubmit={handleSubmit}
           >
-            Submit
-          </button>
-        </form>
+            <Label id="email" label="Email Address" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              value={emailValue}
+              onChange={(e) => handleEmailChange(e)}
+              className={`${baseInputStyle} ${
+                !validEmail ? "focus:ring-red-400" : "focus:ring-blue-400"
+              }`}
+            />
+            {!validEmail && (
+              <ErrorMessage message="Please input a valid email :/" />
+            )}
+            <Label id="password" label="Password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              value={passwordValue}
+              autoComplete="true"
+              className={`${baseInputStyle} ${
+                passwordWeak && "focus:ring-red-400"
+              }`}
+              onChange={(e) => handlePasswordChange(e)}
+            />
+            {passwordWeak && (
+              <ErrorMessage message="A strong password should be 8-15 characters with at least one uppercase, one lowercase, one numeric digit and one special character" />
+            )}
+            <Label id="confirmPassword" label="Confirm Password" />
+            <input
+              type="password"
+              id="confirmPassword"
+              placeholder="Confirm password"
+              autoComplete="true"
+              value={checkPassword}
+              className={`${baseInputStyle} ${
+                !passwordMatch && "focus:ring-red-400"
+              }`}
+              onChange={(e) => handleConfirmPassword(e)}
+            />
+            {!passwordMatch && (
+              <ErrorMessage message="Passwords don't match, please crosscheck." />
+            )}
+            <button
+              type="submit"
+              onClick={(e) => handleSubmit(e)}
+              className="bg-blue-400 w-24 text-center rounded-md p-2 text-white font-bold mt-4 place-self-end"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
